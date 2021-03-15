351949b1e70b643ae419940f02c6b9bb63161823
Nicolas Walcker <walcker_@outlook.com>
2021-03-15 14:55:07 -0300

    Config PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-      return 'PostController index';
+      $posts = Post::get();
+
+      return view('admin.posts.index', compact('posts'));
     }
 }
